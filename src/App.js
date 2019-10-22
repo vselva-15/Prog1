b3ce45269f57f3fc35c9a0aa245556e8ceb2d19c
viji selvaraj <viji.s86@gmail.com>
2019-10-22 16:07:54 +0100

    added newtues js file

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <newtues />
       <contact />
     </div>
   );
---
+++ src/newtues.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function newtues(){
+    return (
+        <div>
+        <h1>myname is leo</h1>
+        <p>email:leo@gmail.com</p>
+        </div>
+    )
+}
+
+export default newtues
