13bc0d069e3e97c724aa0a392b22d5478dae416d
viji selvaraj <viji.s86@gmail.com>
2019-10-22 14:27:26 +0100

    contact js created and contact component added to the app js file

---
+++ src/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function contact(){
+    return (
+        <div>
+        <h1>myname is bob</h1>
+        <p>phone no:233333</p>
+        <p>email: bob@gmail.com</p>
+        </div>
+    )
+}
+
+export default contact
